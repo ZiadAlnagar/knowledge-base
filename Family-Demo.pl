e61dc77699c32748db7227f50f56ca62f497d8b2
ZiadAlnagar <ziadalnagdev@gmail.com>
2021-05-01 12:27:58 +0200

    Family tree

--- Family-Demo.pl
+++ Family-Demo.pl
@@ -2,6 +2,9 @@
 %%%     Author: Ziad Alnagar     %%%
 %%%   Last Updated: 1st May 21   %%%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+% To-do: Uncle, Aunt, Grandparents.
+
 male(tom).
 male(harlan).
 male(joel).
@@ -24,10 +27,6 @@ female(adalyn).
 female(molly).
 female(juliana).
 
-males(L) :- findall(X, male(X), L).
-females(L) :- findall(X, female(X), L).
-people(L) :- findall(X, (male(X) ; female(X)), L).
-
 parent(tom, juliana).
 parent(may, juliana).
 parent(tom, molly).
@@ -40,25 +39,26 @@ parent(juliana, jhon).
 parent(molly, jesson).
 parent(ban, jesson).
 
+% Lists
+males(L) :- findall(X, male(X), L).
+females(L) :- findall(X, female(X), L).
+people(L) :- findall(X, (male(X) ; female(X)), L).
+siblings(L) :- setof([X,Y], (parent(Z, X), parent(Z, Y), \+ X==Y), L).
+
+% find_in_list(X) :- (males(L) ; females(L)), member(X, L).
+is_person(X) :- (people(L)), member(X, L), write(X), write(' exists').
+
 father(X, Y) :- parent(X, Y), male(X).
 mother(X, Y) :- parent(X, Y), female(X).
 % ex: has_children(may, L)
-has_children(X, L) :- findall(Y, parent(X, Y), L), (is_father(X) -> write(X), write(' is father of '), write(L)) ;
-findall(Y, parent(X, Y), L), (is_mother(X) -> write(X), write(' is mother of '), write(L)).
+has_children(X, L) :- findall(Y, parent(X, Y), L), (father(X, _) -> write(X), write(' is father of '), write(L)) ;
+findall(Y, parent(X, Y), L), (mother(X, _) -> write(X), write(' is mother of '), write(L)).
 
-siblings(L) :- setof([X,Y], (parent(Z, X), parent(Z, Y), \+ X==Y), L).
-is_sibling(X,Y) :- siblings(L), member([X,Y], L).
+are_siblings(X,Y) :- siblings(L), member([X,Y], L).
 brother(X, Y) :- sibling(X, Y), male(X), write(X), write(' is brother to '), write(Y).
 sister(X, Y) :- sibling(X, Y), female(X), write(X), write(' is sister to '), write(Y).
 
-% To-do: Uncle, Aunt, Grandparents.
 
-%find_in_list(X) :- (males(L) ; females(L)), member(X, L).
-is_person(X) :- (people(L)), member(X, L), write(X), write(' exists').
-
-% Helper predicates
-is_father(X) :- parent(X, _), male(X).
-is_mother(X) :- parent(X, _), female(X).
 
 
 
